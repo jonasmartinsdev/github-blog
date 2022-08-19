dfd51e4c50e4dfdf39cb234fd99b2238ff79a0a7
jonasmartins <jonasmartinsrubim@gmail.com>
2022-08-19 18:26:00 -0300

    desafio finalizado :tada:

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const BackgroundHeader = styled.header`
-  background: url('/public/cover.svg');
+  background: url('/cover.svg');
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
